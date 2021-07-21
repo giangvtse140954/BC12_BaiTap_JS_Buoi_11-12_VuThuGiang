098a57d7c8d56a3908d352126d14667cc25e20e8
Vu Thu Giang <vuthugiang2610@gmail.com>
2021-07-21 19:00:33 +0700

    aaa

--- admin/app/services/UserService.js
+++ admin/app/services/UserService.js
@@ -1,47 +1,47 @@
-function UserService(){
-    this.getAllUsers = function(){
-        return axios({
-            url: 'https://60f52fd32208920017f39f5a.mockapi.io/users',
-            method: 'GET',
-        });
-    };
+function UserService() {
+  this.getAllUsers = function () {
+    return axios({
+      url: 'https://60f444843cb0870017a8a1b4.mockapi.io/users',
+      method: 'GET',
+    });
+  };
 
-    this.addNewUser = function(user){
-        return axios({
-            url: "https://60f52fd32208920017f39f5a.mockapi.io/users",
-            method: "POST",
-            data: user
-        });
-    };
+  this.addNewUser = function (user) {
+    return axios({
+      url: 'https://60f444843cb0870017a8a1b4.mockapi.io/users',
+      method: 'POST',
+      data: user,
+    });
+  };
 
-    this.getUserById = function(id){
-        return axios({
-            url:`https://60f52fd32208920017f39f5a.mockapi.io/users/${id}`,
-            method: "GET",
-        });
-    };
+  this.getUserById = function (id) {
+    return axios({
+      url: `https://60f444843cb0870017a8a1b4.mockapi.io/users/${id}`,
+      method: 'GET',
+    });
+  };
 
-    this.updateUser = function(user,id){
-        return axios({
-            url: `https://60f52fd32208920017f39f5a.mockapi.io/users/${id}`,
-            method: "PUT",
-            data: user
-        })
-    };
+  this.updateUser = function (user, id) {
+    return axios({
+      url: `https://60f444843cb0870017a8a1b4.mockapi.io/users/${id}`,
+      method: 'PUT',
+      data: user,
+    });
+  };
 
-    this.deleteUser = function(id){
-        return axios({
-            url: `https://60f52fd32208920017f39f5a.mockapi.io/users/${id}`,
-            method: "DELETE",
-        })
-    };
+  this.deleteUser = function (id) {
+    return axios({
+      url: `https://60f444843cb0870017a8a1b4.mockapi.io/users/${id}`,
+      method: 'DELETE',
+    });
+  };
 
-    this.checkAccountDuplicated = function(account, userArr){
-        for(let i = 0 ; i < userArr.length; i++){
-            if(userArr[i].taiKhoan.toLowerCase() == account.toLowerCase()){
-                return true;
-            } 
-        }
-        return false;
+  this.checkAccountDuplicated = function (account, userArr) {
+    for (let i = 0; i < userArr.length; i++) {
+      if (userArr[i].taiKhoan.toLowerCase() == account.toLowerCase()) {
+        return true;
+      }
     }
+    return false;
+  };
 }
